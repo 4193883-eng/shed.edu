d1709914c19de41e0d7050bb22778f38227a4f65
Dimon1511 <dimatis0911@gmail.com>
2024-03-23 15:01:15 +0200

    add icons to sidebar

--- src/components/rootLayoutComponents/Sidebar.jsx
+++ src/components/rootLayoutComponents/Sidebar.jsx
@@ -12,7 +12,13 @@ import {
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { SiteLogo } from '../SiteLogo.jsx';
+import { FaHome } from 'react-icons/fa';
+import { FaAddressCard } from 'react-icons/fa';
+import { FaArrowRightFromBracket, FaNewspaper } from 'react-icons/fa6';
 import { IoClose } from 'react-icons/io5';
+import { PiSignInFill } from 'react-icons/pi';
+import { FaArrowRightToBracket } from 'react-icons/fa6';
+
 import { Link } from 'react-router-dom';
 import { flushAuthTokenAction } from '../../redux/auth/authActions.js';
 
@@ -40,27 +46,71 @@ export function Sidebar({ isOpen = false, onClose }) {
         </DrawerHeader>
         <DrawerBody>
           <Flex flexDirection={'column'}>
-            <Button marginBottom={'10px'} as={Link} to={'/'}>
-              Home page
+            <Button
+              leftIcon={<FaHome size={'25px'} />}
+              fontWeight={'400'}
+              fontFamily={'B612 Mono", monospace'}
+              sizeIcon={'10'}
+              marginBottom={'10pxС'}
+              paddingLeft={'10px'}
+              justifyContent={'start'}
+              variant="ghost"
+              as={Link}
+              to={'/'}
+            >
+              Home
             </Button>
-            <Button marginBottom={'10px'} as={Link} to={'/signin'}>
+            <Button
+              leftIcon={<FaAddressCard size={'25px'} />}
+              fontWeight={'400'}
+              fontFamily={'B612 Mono", monospace'}
+              marginBottom={'10px'}
+              paddingLeft={'10px'}
+              justifyContent={'start'}
+              variant="ghost"
+              as={Link}
+              to={'/signup'}
+            >
+              Register
+            </Button>
+            <Button
+              leftIcon={<FaArrowRightToBracket size={'25px'} />}
+              fontWeight={'400'}
+              fontFamily={'B612 Mono", monospace'}
+              marginBottom={'10px'}
+              paddingLeft={'10px'}
+              justifyContent={'start'}
+              variant="ghost"
+              as={Link}
+              to={'/signin'}
+            >
               Log in
             </Button>
-            <Button as={Link} to={'/news'}>
+            <Button
+              leftIcon={<FaNewspaper size={'25px'} />}
+              fontWeight={'400'}
+              fontFamily={'B612 Mono", monospace'}
+              marginBottom={'10px'}
+              paddingLeft={'10px'}
+              justifyContent={'start'}
+              variant="ghost"
+              as={Link}
+              to={'/news'}
+            >
               News
             </Button>
           </Flex>
         </DrawerBody>
         <DrawerFooter>
-          <Button
+          <IconButton
             onClick={() => {
               dispatch(flushAuthTokenAction());
             }}
+            variant="outline"
+            icon={<FaArrowRightFromBracket size={'20px'} />}
             as={Link}
             to={'/'}
-          >
-            Log out
-          </Button>
+          ></IconButton>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
