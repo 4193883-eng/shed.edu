700e93a3b7d77721d537f799c7c85f579191df31
Danya <skrepecda998@gmail.com>
2024-04-11 20:31:10 +0300

    Howe work create modal

--- src/pages/HomeworkListPage.jsx
+++ src/pages/HomeworkListPage.jsx
@@ -1,4 +1,4 @@
-import { BsThreeDotsVertical } from 'react-icons/bs';
+import { BsFileEarmarkImage, BsThreeDotsVertical } from 'react-icons/bs';
 import {
   Flex,
   MenuItem,
@@ -14,113 +14,226 @@ import {
   Text,
   Heading,
   List,
+  FormControl,
+  FormLabel,
+  Select,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
 } from '@chakra-ui/react';
+import { InputField } from '../components/auth/InputField';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import { DateInput } from '../components/auth/DateInput';
+import { useState } from 'react';
 
+const validationSchema = yup.object().shape({
+  title: yup.string().min(3, 'Title is too short').max(50, 'Title is too long'),
+  description: yup.string().max(300, 'Description is too long'),
+  dueDate: yup.date(),
+  subjectId: yup.number().positive().integer(),
+});
 export function HomeworkListPage() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [hw, setHw] = useState(null);
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      description: '',
+      dueDate: '',
+      subjectId: 0,
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      createHomeWorkService({
+        title: values.title,
+        description: values.description,
+        dueDate: values.dueDate,
+        subjectId: values.subjectId,
+      })
+        // .catch((err) => {
+        //   setError(err);
+        // })
+        .then((sub) => {
+          setHw((prev) => [...prev, sub]);
+          fetchSubjects();
+        });
+      // .finally(() => setFormLoading(false));
+    },
+  });
+
   return (
-    <Flex
-      justifyContent={'center'}
-      flexDir={'column'}
-      alignItems={'center'}
-      pt={16}
-      width={'100%'}
-      maxW={'700px'}
-      gap={4}
-      m={'auto'}
-      mt={4}
-    >
-      <Heading>HomeWork List</Heading>
-
-      <List
+    <div>
+      <Flex
+        justifyContent={'center'}
+        flexDir={'column'}
         alignItems={'center'}
-        flexDirection={'column'}
-        display={'flex'}
+        pt={16}
         width={'100%'}
-        gap={'4'}
+        maxW={'700px'}
+        gap={4}
+        m={'auto'}
+        mt={4}
       >
-        <ListItem maxW={'500px'} w={'100%'}>
-          <Card>
-            <CardBody
-              alignItems={'center'}
-              justifyContent={'space-between'}
-              p={'5'}
-              display={'flex'}
-            >
-              <Text>subeject of the homework</Text>
-              <Text>Due date</Text>
-              <Menu>
-                <MenuButton
-                  as={IconButton}
-                  variant="ghost"
-                  colorScheme="gray"
-                  aria-label="See menu"
-                  icon={<BsThreeDotsVertical />}
+        <Heading>HomeWork List</Heading>
+        <Box>
+          <Button type="submit" mt={2} onClick={onOpen}>
+            Create Homework
+          </Button>
+          <Modal onClose={onClose} size="xl" isOpen={isOpen}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Create</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                {' '}
+                <InputField
+                  meta={formik.getFieldMeta('title')}
+                  label={'Title'}
+                  required={true}
+                  placeholder={'Title'}
+                  disabled={false}
+                  {...formik.getFieldProps('title')}
                 />
-
-                <MenuList>
-                  <MenuItem>Edit</MenuItem>
-                  <MenuItem color="red.400">Delete</MenuItem>
-                </MenuList>
-              </Menu>
-            </CardBody>
-          </Card>
-        </ListItem>
-        <ListItem maxW={'500px'} w={'100%'}>
-          <Card>
-            <CardBody
-              alignItems={'center'}
-              justifyContent={'space-between'}
-              p={'5'}
-              display={'flex'}
-            >
-              <Text>subeject of the homework</Text>
-              <Text>Due date</Text>
-
-              <Menu>
-                <MenuButton
-                  as={IconButton}
-                  variant="ghost"
-                  colorScheme="gray"
-                  aria-label="See menu"
-                  icon={<BsThreeDotsVertical />}
+                <InputField
+                  meta={formik.getFieldMeta('description')}
+                  label={'Description'}
+                  required={true}
+                  placeholder={'Description'}
+                  disabled={false}
+                  {...formik.getFieldProps('description')}
                 />
-
-                <MenuList>
-                  <MenuItem>Edit</MenuItem>
-                  <MenuItem color="red.400">Delete</MenuItem>
-                </MenuList>
-              </Menu>
-            </CardBody>
-          </Card>
-        </ListItem>
-        <ListItem maxW={'500px'} w={'100%'}>
-          <Card>
-            <CardBody
-              alignItems={'center'}
-              justifyContent={'space-between'}
-              p={'5'}
-              display={'flex'}
-            >
-              <Text>subeject of the homework</Text>
-              <Text>Due date</Text>
-              <Menu>
-                <MenuButton
-                  as={IconButton}
-                  variant="ghost"
-                  colorScheme="gray"
-                  aria-label="See menu"
-                  icon={<BsThreeDotsVertical />}
+                <DateInput
+                  placeholder="Date"
+                  meta={formik.getFieldMeta('dueDate')}
+                  label={'Date'}
+                  required={true}
+                  disabled={false}
+                  {...formik.getFieldProps('dueDate')}
                 />
+                <FormControl
+                  isRequired={true}
+                  isDisabled={false}
+                  isInvalid={
+                    !!formik.getFieldMeta('subjectId').error &&
+                    formik.getFieldMeta('subjectId').touched
+                  }
+                >
+                  <FormLabel>Subject</FormLabel>
+                  <Select
+                    placeholder="Select subject"
+                    {...formik.getFieldProps('subjectId')}
+                  >
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                    <option value="option3">Option 3</option>
+                  </Select>
+                </FormControl>
+              </ModalBody>
+              <ModalFooter>
+                <Button type="submit" m={'auto'}>
+                  Create Homework
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </Box>
+        <List
+          alignItems={'center'}
+          flexDirection={'column'}
+          display={'flex'}
+          width={'100%'}
+          gap={'4'}
+        >
+          <ListItem maxW={'500px'} w={'100%'}>
+            <Card>
+              <CardBody
+                alignItems={'center'}
+                justifyContent={'space-between'}
+                p={'5'}
+                display={'flex'}
+              >
+                <Text>subeject of the homework</Text>
+                <Text>Due date</Text>
+                <Menu>
+                  <MenuButton
+                    as={IconButton}
+                    variant="ghost"
+                    colorScheme="gray"
+                    aria-label="See menu"
+                    icon={<BsThreeDotsVertical />}
+                  />
 
-                <MenuList>
-                  <MenuItem>Edit</MenuItem>
-                  <MenuItem color="red.400">Delete</MenuItem>
-                </MenuList>
-              </Menu>
-            </CardBody>
-          </Card>
-        </ListItem>
-      </List>
-    </Flex>
+                  <MenuList>
+                    <MenuItem>Edit</MenuItem>
+                    <MenuItem color="red.400">Delete</MenuItem>
+                  </MenuList>
+                </Menu>
+              </CardBody>
+            </Card>
+          </ListItem>
+          <ListItem maxW={'500px'} w={'100%'}>
+            <Card>
+              <CardBody
+                alignItems={'center'}
+                justifyContent={'space-between'}
+                p={'5'}
+                display={'flex'}
+              >
+                <Text>subeject of the homework</Text>
+                <Text>Due date</Text>
+
+                <Menu>
+                  <MenuButton
+                    as={IconButton}
+                    variant="ghost"
+                    colorScheme="gray"
+                    aria-label="See menu"
+                    icon={<BsThreeDotsVertical />}
+                  />
+
+                  <MenuList>
+                    <MenuItem>Edit</MenuItem>
+                    <MenuItem color="red.400">Delete</MenuItem>
+                  </MenuList>
+                </Menu>
+              </CardBody>
+            </Card>
+          </ListItem>
+          <ListItem maxW={'500px'} w={'100%'}>
+            <Card>
+              <CardBody
+                alignItems={'center'}
+                justifyContent={'space-between'}
+                p={'5'}
+                display={'flex'}
+              >
+                <Text>subeject of the homework</Text>
+                <Text>Due date</Text>
+                <Menu>
+                  <MenuButton
+                    as={IconButton}
+                    variant="ghost"
+                    colorScheme="gray"
+                    aria-label="See menu"
+                    icon={<BsThreeDotsVertical />}
+                  />
+
+                  <MenuList>
+                    <MenuItem>Edit</MenuItem>
+                    <MenuItem color="red.400">Delete</MenuItem>
+                  </MenuList>
+                </Menu>
+              </CardBody>
+            </Card>
+          </ListItem>
+        </List>
+      </Flex>
+    </div>
   );
 }
