5abd6665b37001f4fe704c3ec3a0c6e590effe1d
4193883-eng <83756034+4193883-eng@users.noreply.github.com>
2024-04-16 20:04:49 +0300

    Various changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,22 @@
-import {Flex} from "@chakra-ui/react";
+import {Box, Divider, Flex, Heading, IconButton} from "@chakra-ui/react";
+import {HiPlusSm} from "react-icons/hi";
 
 function HomePage() {
-  return <Flex justifyContent={'center'} flexDir={'column'} alignItems={'center'} pt={16}>
-  </Flex>;
+  return <Flex
+    justifyContent={'center'}
+    flexDir={'column'}
+    alignItems={'center'}
+    pt={16}
+    width={'100%'}
+    maxW={'700px'}
+    gap={4}
+    m={'auto'}
+    mt={0}
+    p={4}
+  >
+    <Heading alignSelf={'start'}>Home</Heading>
+    <Divider />
+  </Flex>
 }
 
 export default HomePage;
--- src/pages/SchedulePage.jsx
+++ src/pages/SchedulePage.jsx
@@ -1,4 +1,5 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import {Box, Button, Divider, Flex, Heading, IconButton} from '@chakra-ui/react';
+import {HiPlusSm} from "react-icons/hi";
 
 export function SchedulePage() {
   return (
@@ -15,16 +16,52 @@ export function SchedulePage() {
         mt={0}
         p={4}
       >
-        <Heading alignSelf={'start'}>Subject List</Heading>
+        <Heading alignSelf={'start'}>Schedule</Heading>
+        <Divider />
+        <Box
+          display={'flex'}
+          flexDir={'column'}
+          gap={2}
+          p={2}
+          maxW={'300px'}
+          width={'100%'}
+        >
+          <Box
+            display={'flex'}
+            justifyContent={'space-between'}
+            alignItems={'center'}
+            p={4}
+            bg={'purple.50'}
+            width={'100%'}
+            borderRadius={'md'}
+            color={'purple.700'}
+            transition={'0.3s'}
+            _hover={{
+              bg: 'purple.100',
+              color: 'purple.800'
+            }}
+          >
+            Monday
+            <IconButton variant={'ghost'} colorScheme={'purple'} aria-label={'add a subject'} icon={<HiPlusSm />}/>
+          </Box>
+          <Box
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            py={4}
+            bg={'gray.50'}
+            width={'100%'}
+            borderRadius={'md'}
+           >
+            Math
+
+          </Box>
+        </Box>
       </Flex>
     </>
   );
 }
 
-function SubjectDisplay({}) {
+function SubjectDisplay({}) {}
 
-}
-
-function DayScheduleDisplay({daySchedule}) {
-
-}
+function DayScheduleDisplay({ daySchedule }) {}
