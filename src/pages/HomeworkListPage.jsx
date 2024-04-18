52cb157adf9ed325f00f748ee3af6cbeacae15e8
Dimon1511 <dimatis0911@gmail.com>
2024-04-18 21:24:32 +0300

    ujpoiu;oiuuiyhipou

--- src/pages/HomeworkListPage.jsx
+++ src/pages/HomeworkListPage.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import {
   Image,
   Flex,
@@ -35,25 +35,14 @@ import { InputField } from '../components/auth/InputField';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
 import { DateInput } from '../components/auth/DateInput';
-import { useState } from 'react';
+
 import {
   getAllHomeworksService,
   createHomeworkService,
+  deleteHomeworkService,
 } from '../services/homeWorksServices';
-import { useEffect } from 'react';
-import { getAllSubjectsService } from '../services/subjectsServices';
 
-export const LIST_ITEMS = [
-  {
-    title: 'good',
-    dueDate: '04.08.2222',
-    description: 'bad',
-  },
-  {
-    title: 'dwef',
-    description: 'bawfaefd',
-  },
-];
+import { getAllSubjectsService } from '../services/subjectsServices';
 
 const validationSchema = yup.object().shape({
   title: yup.string().min(3, 'Title is too short').max(50, 'Title is too long'),
@@ -61,6 +50,7 @@ const validationSchema = yup.object().shape({
   dueDate: yup.date(),
   subjectId: yup.number().positive().integer(),
 });
+
 export function HomeworkListPage() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [hws, setHws] = useState(null);
@@ -103,6 +93,23 @@ export function HomeworkListPage() {
       .finally(() => {
         setLoading(false);
       });
+  }
+
+  function onEdit(id, hw) {
+    setHws((prev) => {
+      return prev.map((prevHw) => {
+        return id === prevHw.id ? hw : prevHw;
+      });
+    });
+  }
+
+  function onDelete(id) {
+    setHws((prev) => {
+      return prev.filter((prevId) => {
+        return prevId !== id;
+      });
+    });
+    fetchHomeworks(onEdit);
   }
   function fetchSubjects() {
     setLoading(true);
@@ -216,6 +223,8 @@ export function HomeworkListPage() {
             <UnorderedList width={'100%'} maxW={'500px'}>
               {hws.map((hw) => (
                 <HomeworkListItem
+                  onEdit={onEdit}
+                  onDelete={onDelete}
                   title={hw.title}
                   description={hw.description}
                   id={hw.id}
@@ -236,6 +245,8 @@ export function HomeworkListPage() {
 }
 
 function HomeworkListItem({
+  onEdit,
+  onDelete,
   title,
   description,
   id,
@@ -245,22 +256,122 @@ function HomeworkListItem({
   dueDate,
   subjectId,
 }) {
+  const editingModal = useDisclosure();
+
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const moreButton = useRef();
-  const icon = useRef();
+  const cardBody = useRef();
+  const [subjects, setSubjects] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleOpening(e) {
-    if (e.target != moreButton.current && e.target != icon.current) {
+    if (e.target == cardBody.current) {
       onOpen();
     }
   }
 
+  function handleDeleting() {
+    setIsLoading(true);
+    deleteHomeworkService(id).then(() => {
+      onDelete(id);
+      setIsLoading(false);
+    });
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      title: title,
+      description: description,
+      dueDate: dueDate,
+      subjectId: subjectId,
+    },
+    validationSchema,
+    onSubmit: (values) => {},
+  });
+
+  useEffect(() => {
+    getAllSubjectsService().then((data) => {
+      setSubjects(data);
+    });
+  }, []);
+
   return (
     <>
+      <Modal
+        onClose={editingModal.onClose}
+        isOpen={editingModal.isOpen}
+        size="xl"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {' '}
+            <Box as="form">
+              <InputField
+                meta={formik.getFieldMeta('title')}
+                label={'Title'}
+                required={true}
+                placeholder={'Title'}
+                disabled={false}
+                {...formik.getFieldProps('title')}
+              />
+
+              <InputField
+                meta={formik.getFieldMeta('description')}
+                label={'Description'}
+                required={true}
+                placeholder={'Description'}
+                disabled={false}
+                {...formik.getFieldProps('description')}
+              />
+              <DateInput
+                placeholder="Date"
+                meta={formik.getFieldMeta('dueDate')}
+                label={'Date'}
+                required={true}
+                disabled={false}
+                {...formik.getFieldProps('dueDate')}
+              />
+              <FormControl
+                isRequired={true}
+                isDisabled={false}
+                isInvalid={
+                  !!formik.getFieldMeta('subjectId').error &&
+                  formik.getFieldMeta('subjectId').touched
+                }
+              >
+                <FormLabel>Subject</FormLabel>
+                <Select
+                  placeholder="Select subject"
+                  {...formik.getFieldProps('subjectId')}
+                >
+                  {!!subjects &&
+                    subjects.sort().map((subject) => {
+                      return (
+                        <option value={subject.id} key={subject.id}>
+                          {subject.name}
+                        </option>
+                      );
+                    })}
+                </Select>
+              </FormControl>
+            </Box>
+          </ModalBody>
+          <ModalFooter>
+            <Box as="form" onSubmit={formik.handleSubmit}>
+              <Button type="submit" m={'auto'}>
+                Create Homework
+              </Button>
+            </Box>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
       <Modal onClose={onClose} size={'xl'} isOpen={isOpen}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader> Viewing {title}</ModalHeader>
           <ModalCloseButton />
           <ModalBody></ModalBody>
           <ModalFooter>
@@ -268,10 +379,10 @@ function HomeworkListItem({
           </ModalFooter>
         </ModalContent>
       </Modal>
-
       <ListItem maxW={'500px'} w={'100%'}>
         <Card>
           <CardBody
+            ref={cardBody}
             onClick={handleOpening}
             alignItems={'center'}
             justifyContent={'space-between'}
@@ -283,7 +394,8 @@ function HomeworkListItem({
 
             <Menu>
               <MenuButton
-                ref={moreButton}
+                isLoading={isLoading}
+                // ref={moreButton}
                 as={IconButton}
                 variant="ghost"
                 colorScheme="gray"
@@ -291,15 +403,26 @@ function HomeworkListItem({
                 icon={
                   <Image
                     src="/dots.svg"
-                    ref={icon}
+                    // ref={icon}
                     transform={'rotate(90deg)'}
                   />
                 }
               />
 
               <MenuList>
-                <MenuItem>Edit</MenuItem>
-                <MenuItem color="red.400">Delete</MenuItem>
+                <MenuItem
+                  //  ref={edit}
+                  onClick={editingModal.onOpen}
+                >
+                  Edit
+                </MenuItem>
+                <MenuItem
+                  // ref={deleteHw}
+                  color="red.400"
+                  onClick={handleDeleting}
+                >
+                  Delete
+                </MenuItem>
               </MenuList>
             </Menu>
           </CardBody>
@@ -310,6 +433,8 @@ function HomeworkListItem({
 }
 
 HomeworkListItem.propTypes = {
+  onEdit: PropTypes.func,
+  onDelete: PropTypes.func,
   id: PropTypes.number,
   createdAt: PropTypes.string,
   updatedAt: PropTypes.string,
